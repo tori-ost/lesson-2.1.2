1dbbd1cc8cc36df28d73ca1b0ff2d4592d84b33e
Viktoriya-Astapuk <viktoryia.astapuk@gmail.com>
2024-03-09 16:37:18 +0300

    added task_2.1.5

---
+++ task_2.1.5.js
@@ -0,0 +1,14 @@
+const value1 = 9;
+let value2 = Math.floor(Math.random() * 30);
+
+console.log(value2);
+
+result = value1 + value2 / 5 + 17;
+
+console.log(result);
+
+/* В нашем примере приоритет операторов следующий:
+1. Оператор / (деление)
+2. Оператор + (сложение)
+3. Оператор = (присваивание)
+*/
