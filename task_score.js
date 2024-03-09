07df2081796414fc87ddcbddbcac3dc774d80f31
Viktoriya-Astapuk <viktoryia.astapuk@gmail.com>
2024-03-09 16:09:56 +0300

    added task_score.js for task 2.1.3

---
+++ task_score.js
@@ -0,0 +1,11 @@
+let introScore = 50;
+let gitScore = 45;
+let jsScore = 100;
+let avgScore = (introScore + gitScore + jsScore) / 3;
+
+console.log(`Средний балл по трем модулям равен ${avgScore}`);
+
+let firstName = "Коля";
+let lastName = "Сидоров";
+
+console.log(`${firstName} ${lastName}: средний балл по трем модулям равен ${avgScore}`);
